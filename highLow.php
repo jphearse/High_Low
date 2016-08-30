207e7cc860d8db94f54f061d75403bdc9e1c9c50
Joe Phearse <jphearse@gmail.com>
2016-08-30 10:21:29 -0500

    created high low game

--- highLow.php
+++ highLow.php
@@ -1,2 +1,28 @@
 <?php 
 
+$randomNumber = mt_rand(1,100);
+echo $randomNumber. PHP_EOL;
+
+fwrite(STDOUT, "What is your guess?\n");
+$userGuess = trim(fgets(STDIN));
+$count = 1;
+
+do {
+
+	if ( $userGuess < $randomNumber){
+
+		echo 'HIGHER' . PHP_EOL;
+		fwrite(STDOUT, "What is your guess?\n");
+		$userGuess = trim(fgets(STDIN));
+
+	} elseif ( $userGuess > $randomNumber){
+		
+		echo 'LOWER' . PHP_EOL;
+		fwrite(STDOUT, "What is your guess?\n");
+		$userGuess = trim(fgets(STDIN));
+
+	}
+	$count++;
+} while( $userGuess != $randomNumber);
+echo "GOOD GUESS!" . PHP_EOL;
+echo "Rounds $count" . PHP_EOL;

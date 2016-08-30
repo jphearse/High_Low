37aac505aa0a2758f6026adea4e84bb2f6340506
Joe Phearse <jphearse@gmail.com>
2016-08-30 12:08:36 -0500

    added additional features

--- highLow.php
+++ highLow.php
@@ -1,28 +1,55 @@
 <?php 
+function highlow(){
+	fwrite(STDOUT, "Input a range\nFirst Number:"); //input first number
+	$min = floatval(fgets(STDIN));
+	fwrite(STDOUT, "Second Number:"); //input second number
+	$max = floatval(fgets(STDIN));
 
-$randomNumber = mt_rand(1,100);
-echo $randomNumber. PHP_EOL;
+	$randomNumber = mt_rand($min,$max);  //random number between min and max
+	echo $randomNumber. PHP_EOL;
 
-fwrite(STDOUT, "What is your guess?\n");
-$userGuess = trim(fgets(STDIN));
-$count = 1;
+	fwrite(STDOUT, "What is your guess?\n"); //takes users guess
+	$userGuess = trim(fgets(STDIN));
+	$count = 1;
+	$maxRounds = 3;
 
-do {
+do {										//runs this loop while user guess does not equal the number
+	// $number = is_numeric($userGuess);
+	// if ($number == true){
 
+	}
 	if ( $userGuess < $randomNumber){
-
+		$count++;
 		echo 'HIGHER' . PHP_EOL;
 		fwrite(STDOUT, "What is your guess?\n");
 		$userGuess = trim(fgets(STDIN));
 
 	} elseif ( $userGuess > $randomNumber){
-		
+		$count++;
 		echo 'LOWER' . PHP_EOL;
 		fwrite(STDOUT, "What is your guess?\n");
 		$userGuess = trim(fgets(STDIN));
 
 	}
-	$count++;
+	
 } while( $userGuess != $randomNumber);
-echo "GOOD GUESS!" . PHP_EOL;
-echo "Rounds $count" . PHP_EOL;
+
+if($userGuess == $randomNumber){			//user gets number right
+	echo "GOOD GUESS!" . PHP_EOL; 
+} 
+
+echo "Rounds $count" . PHP_EOL;			//outputs numebrs of rounds played
+
+fwrite(STDOUT, "Would you like to play again? (yes/no) ");		//play again?
+	$playAgain = trim(fgets(STDIN));
+	if($playAgain == "yes"){
+		highlow();
+	}
+}
+highlow();
+
+
+
+
+
+
